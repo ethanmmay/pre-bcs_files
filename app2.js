6235b885945acb925a4a56b36c00fe728578e792
Ethan May <ethaniel.may@gmail.com>
2020-12-22 19:21:42 -0700

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,26 @@
+let num1 = 10
+let num2 = 20
+
+if (num1 < num2) {
+    console.log("Number 1 is less than Number 2")
+} else if (num1 > num2) {
+    console.log("Number 1 is greater than Number 2")
+} else {
+    console.log("Number 1 is the same as Number 2")
+}
+
+let letter = "n"
+
+switch (letter) {
+    case "n":
+        console.log("n")
+        break;
+    case "N":
+        console.log("N")
+        break;
+    case "a":
+        console.log("a")
+        break;
+    
+
+}
