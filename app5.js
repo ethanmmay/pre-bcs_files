72e495a5c6153e5b2787a4b0cb8554d810ad2071
Ethan May <ethaniel.may@gmail.com>
2020-12-22 19:38:47 -0700

    Add files via upload

---
+++ app5.js
@@ -0,0 +1,18 @@
+let pencil = "pencil"
+let pen = "pen"
+let quill = "quill"
+
+let writing_utensils = []
+
+writing_utensils.push("pencil")
+writing_utensils.push(pen)
+writing_utensils.push(quill)
+
+// 0 = pencil
+// 1 = pen
+// 2 = quill
+
+writing_utensils[0] // "pencil"
+writing_utensils[1] // "pen"
+writing_utensils[2] // "quill"
+
