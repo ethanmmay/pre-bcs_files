3f5f6a6bf5ecfca0c38571b54f24d7e58ae63ef1
Ethan May <ethaniel.may@gmail.com>
2020-12-23 15:33:53 -0700

    Test Message

--- app6.js
+++ app6.js
@@ -2,4 +2,34 @@ for (let count = 0; count <= 10; count++) {
     console.log(count)
 }
 
-// Test Line for GitHub
+let number = 13
+while (number <= 10) {
+    console.log(number)
+    number++
+}
+
+let counter = 14
+do {
+    console.log(counter) // Printed 14
+    counter++ // Now 15
+} while (counter <= 10) // Doesn't Run Again
+
+let people = [
+    {
+        name: "Ethan",
+        email: "ethaniel.may@gmail.com"
+    },
+    {
+        name: "Geralt",
+        email: "geraltofrivia@gmail.com"
+    },
+    {
+        name: "Triss",
+        email: "trissmarigold@gmail.com"
+    }
+]
+
+for (let i = 0; i < people.length; i++) {
+    let person = people[i]
+    console.log("Name: ", person.name, "\nEmail: ", person.email)
+}
---
+++ app7.js
@@ -0,0 +1,9 @@
+let testnum 
+
+function greeting(testnum) {
+    console.log("test " + testnum)
+}
+
+for (let i = 0; i < 10; i++) {
+    greeting(i)
+}
