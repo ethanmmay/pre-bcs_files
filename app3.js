3ff823ca88929efaae7a50f470378bdea53e238b
Ethan May <ethaniel.may@gmail.com>
2020-12-22 19:35:44 -0700

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,8 @@
+console.log(document)
+console.log(document.getElementById('name'))
+
+let person = {
+    name: "Darryl"
+}
+
+document.getElementById('name').innerText = "Mark"
---
+++ app4.js
@@ -0,0 +1,13 @@
+let me = {
+    name: "Ethan",
+    age: 18,
+    eyeColor: "Brown"
+}
+
+me.age // 18
+me["age"]
+
+me.name // "Ethan"
+me["name"]
+let key = "eyeColor"
+me[key] // Brown
